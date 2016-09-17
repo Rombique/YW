c0e28685b2458d3230e61646e9e2bc4397f3c161
Rombique <rombique@gmail.com>
2016-09-17 21:24:00 +0300

    Add DataBase, DataGrid, GroupBox with labels and buttons

--- YourWallet/YourWallet/MainWindow.xaml.cs
+++ YourWallet/YourWallet/MainWindow.xaml.cs
@@ -24,5 +24,16 @@ namespace YourWallet
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            YourWallet.dbDataSet dbDataSet = ((YourWallet.dbDataSet)(this.FindResource("dbDataSet")));
+            // Load data into the table Table. You can modify this code as needed.
+            YourWallet.dbDataSetTableAdapters.TableTableAdapter dbDataSetTableTableAdapter = new YourWallet.dbDataSetTableAdapters.TableTableAdapter();
+            dbDataSetTableTableAdapter.Fill(dbDataSet.Table);
+            System.Windows.Data.CollectionViewSource tableViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("tableViewSource")));
+            tableViewSource.View.MoveCurrentToFirst();
+        }
     }
 }
