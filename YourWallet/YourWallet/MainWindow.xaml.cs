e84284811f8cefeccc3e489d95592c5f03339776
Rombique <rombique@gmail.com>
2016-09-18 01:58:36 +0300

    Delete 1st proj, create new - MVC

--- YourWallet/YourWallet/MainWindow.xaml.cs
+++ YourWallet/YourWallet/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Data.Sql;
 namespace YourWallet
 {
     /// <summary>
@@ -28,12 +28,6 @@ namespace YourWallet
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
 
-            YourWallet.dbDataSet dbDataSet = ((YourWallet.dbDataSet)(this.FindResource("dbDataSet")));
-            // Load data into the table Table. You can modify this code as needed.
-            YourWallet.dbDataSetTableAdapters.TableTableAdapter dbDataSetTableTableAdapter = new YourWallet.dbDataSetTableAdapters.TableTableAdapter();
-            dbDataSetTableTableAdapter.Fill(dbDataSet.Table);
-            System.Windows.Data.CollectionViewSource tableViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("tableViewSource")));
-            tableViewSource.View.MoveCurrentToFirst();
         }
     }
 }
--- YourWallet/YourWallet/Properties/Settings.Designer.cs
+++ YourWallet/YourWallet/Properties/Settings.Designer.cs
@@ -33,5 +33,16 @@ namespace YourWallet.Properties {
                 return ((string)(this["dbConnectionString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\data.mdf;Inte" +
+            "grated Security=True;Connect Timeout=30")]
+        public string dataConnectionString {
+            get {
+                return ((string)(this["dataConnectionString"]));
+            }
+        }
     }
 }
